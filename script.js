3535eb5f852d45f3f9939f94fb218bae4ba338a6
Temuzon <dddaaannniiieeelll528@gmail.com>
2025-12-25 16:31:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const items = document.querySelectorAll(".item");
 
+items.forEach(item => {
+  item.addEventListener("click", (event) => {
+    
+    items.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
