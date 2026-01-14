f27d78f9fac846eb6e5f49e1b46d336e58d9dfe4
Temuzon <dddaaannniiieeelll528@gmail.com>
2026-01-14 15:26:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// border de los itmes activados cuando estan activos
+
+
 const items = document.querySelectorAll(".item");
 
   items.forEach(item => {
@@ -12,6 +15,10 @@ const items = document.querySelectorAll(".item");
     });
   });
 
+
+// barra de navegacion flexible
+
+
 const boton = document.querySelector(
   ".boton-de-ensanche-de-barra-de-navegacion"
 );
@@ -20,3 +27,35 @@ const barra = document.querySelector(".barra-de-navegacion");
 boton.addEventListener("click", () => {
   barra.classList.toggle("expandida");
 });
+
+
+// Guarda secciones 
+
+
+const sections = document.querySelectorAll(".app-section");
+const navItems = document.querySelectorAll(".item");
+
+function showSection(id) {
+  sections.forEach(section => {
+    section.classList.remove("active-section");
+  });
+
+  const target = document.getElementById(id);
+  if (target) {
+    target.classList.add("active-section");
+  }
+
+  window.scrollTo({ top: 0, behavior: "instant" });
+}
+
+navItems.forEach(item => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const targetId = item.getAttribute("href").replace("#", "");
+    showSection(targetId);
+  });
+});
+
+// sección inicial
+showSection("Home");
